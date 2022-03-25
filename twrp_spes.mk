5876acc50befa5e4cbe71f6a352ba50d855e4f8f
Shashank Pandey <64682801+shashankx86@users.noreply.github.com>
2022-03-26 00:22:02 +0530

    Update twrp_spes.mk

--- twrp_spes.mk
+++ twrp_spes.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 #$(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common Omni stuff.
-#$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 #$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
