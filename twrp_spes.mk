673c301480504dd77fc8c5ff32233b5677dfd49c
Shashank Pandey <64682801+shashankx86@users.noreply.github.com>
2022-03-26 20:58:59 +0530

    Update twrp_spes.mk

--- twrp_spes.mk
+++ twrp_spes.mk
@@ -7,6 +7,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
@@ -15,10 +17,13 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := twrp_spes
-PRODUCT_DEVICE := spes
+BOARD_VENDOR := Redmi
 PRODUCT_BRAND := Redmi
+
+PRODUCT_DEVICE := spes
+PRODUCT_NAME := twrp_spes
+PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_MODEL := Redmi Note 11
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := spes
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
